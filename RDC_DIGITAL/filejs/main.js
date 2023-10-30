05a5f1a8aee70327b61e16fc5b7cad76f4825472
silvaamorim <86632340+silvaamorim@users.noreply.github.com>
2023-10-30 14:18:41 -0300

    Add files via upload

---
+++ RDC_DIGITAL/filejs/main.js
@@ -0,0 +1,56 @@
+$("#linhas").change(function() {
+    let linhaatual = document.getElementById("linhas");
+    let linhacel = document.getElementById("linha");
+    let consorcio = document.getElementById("cons");
+    let contrato = document.getElementById("ncontrato");
+    if(linhaatual.value == "7 (Rubi)"){
+        
+        linhacel.style.backgroundColor = "#F62217";
+        linhaatual.style.backgroundColor = "#F62217";
+
+    }
+    else if(linhaatual.value == "10 (Turquesa)"){
+        
+        linhacel.style.backgroundColor = "#40E0D0";
+        linhaatual.style.backgroundColor = "#40E0D0";
+
+    }
+    else if(linhaatual.value == "11 (Coral)"){
+        
+        linhacel.style.backgroundColor = "#FF7F50";
+        linhaatual.style.backgroundColor = "#FF7F50";
+
+    }
+    else if(linhaatual.value == "12 (Safira)"){
+        
+        linhacel.style.backgroundColor = "#385f8f";
+        linhaatual.style.backgroundColor = "#385f8f";
+
+    }
+
+    else if(linhaatual.value == "13 (Jade)"){
+        
+        linhacel.style.backgroundColor = "#00a86b";
+        linhaatual.style.backgroundColor = "#00a86b";
+
+    }
+    else {
+        
+        linhacel.style.backgroundColor = "#FFFFFF";
+        linhaatual.style.backgroundColor = "#FFFFFF";
+
+    }
+
+    fetch("filejson/contrato.json").then((response) => {
+        response.json().then((Dados) => {
+
+            for (let i = 0; i < Dados.Linhas.length; i++){
+                if(linhaatual === Dados.Linhas[i].Linha){
+                    contrato.value=Dados.Linhas[i].Contrato
+                    consorcio.value=Dados.Linhas[i].Consorcio
+
+                }
+            }
+        })
+    })
+});
