ac002460690e7dbaa00248616f4a875dc0bbf07f
silvaamorim <86632340+silvaamorim@users.noreply.github.com>
2023-11-08 15:40:40 -0300

    Update main.js

--- RDC/filejs/main.js
+++ RDC/filejs/main.js
@@ -311,7 +311,7 @@ $(document).ready(function(){
      $(".dados").on("change",".servico",function(){
 
          let id = this.getAttribute('id');
-         let ids = this.closest("input")[0].parentElement.id; 
+         let ids = this.closest("input")[1].parentElement.id; 
          let codvp = document.getElementById(id).value;
 
              fetch("filejson/itenscontrato.json").then((response) => {
